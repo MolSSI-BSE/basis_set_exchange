57a3b71607cafe2574694238492c39ec64a56609
Daniel Smith <malorian@me.com>
2017-12-06 15:21:56 -0500

    First pass at a setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='bse_scratch',
+        version="0.1.0",
+        description='The Quantum Chemistry Basis Set Exchange',
+        author='The Molecular Sciences Software Institute',
+        author_email='bpp4@vt.edu',
+        url="https://github.com/bennybp/bse-scratch",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
