978be58f11b785d37bd8262cf083f1a0675b5165
Benjamin Pritchard <ben@bennyp.org>
2018-01-12 10:53:11 -0500

    Replace preliminary basis set data

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,12 +5,11 @@ Tests for the BSE exposed API
 import bse
 import pytest
 
-@pytest.mark.parametrize("basis_name,elements", [("6-31G", None),
-                                                 ("6-31G", [1,2,3,4,5]),
-                                                 ("6-31GSS", [6,7,8,9,10]),
-                                                 ("6-31PPGSS-AGG", None),
-                                                 ("LANL2DZ", None),
-                                                 ("LANL2DZ", [24,25,26,92])])
+@pytest.mark.parametrize("basis_name,elements", [("STO-3G", None),
+                                                 ("STO-3G", [1,2,3,4,5]),
+                                                 ("STO-3G", [6,7,8,9,10]),
+                                                 ("STO-2G", None),
+                                                 ("STO-6G", None)])
 def test_get_basis_set(basis_name, elements):
     bse.get_basis_set(basis_name, elements=elements)
 
@@ -32,22 +31,22 @@ def test_get_basis_set(basis_name, elements):
                               uncontract_spdf=unc[0])
 
 
-@pytest.mark.parametrize("basis_name,unc",
-                         [("LANL2DZ", (False, False, False)),
-                          ("LANL2DZ", (False, False, True)),
-                          ("LANL2DZ", (False, True, False)),
-                          ("LANL2DZ", (False, True, True)),
-                          ("LANL2DZ", (True, False, False)),
-                          ("LANL2DZ", (True, False, True)),
-                          ("LANL2DZ", (True, True, False)),
-                          ("LANL2DZ", (True, True, True))])
-def test_get_basis_set_uncontract(basis_name, unc):
-    bse.get_basis_set(basis_name,
-                      uncontract_general=unc[0],
-                      uncontract_segmented=unc[1],
-                      uncontract_spdf=unc[2])
-
-
+#@pytest.mark.parametrize("basis_name,unc",
+#                         [("LANL2DZ", (False, False, False)),
+#                          ("LANL2DZ", (False, False, True)),
+#                          ("LANL2DZ", (False, True, False)),
+#                          ("LANL2DZ", (False, True, True)),
+#                          ("LANL2DZ", (True, False, False)),
+#                          ("LANL2DZ", (True, False, True)),
+#                          ("LANL2DZ", (True, True, False)),
+#                          ("LANL2DZ", (True, True, True))])
+#def test_get_basis_set_uncontract(basis_name, unc):
+#    bse.get_basis_set(basis_name,
+#                      uncontract_general=unc[0],
+#                      uncontract_segmented=unc[1],
+#                      uncontract_spdf=unc[2])
+#
+#
 
 
 @pytest.mark.parametrize("keys", [None])
--- tests/test_converters.py
+++ tests/test_converters.py
@@ -5,11 +5,11 @@ Tests for converting to basis set formats
 import bse
 import pytest
 
-@pytest.mark.parametrize("basis_name", ["6-31G", "6-31GSS", "6-31PPGSS-AGG", "LANL2DZ"])
-def test_converters(basis_name):
-
-    formats = bse.converters.converter_map
-    bs = bse.get_basis_set(basis_name)
-
-    for name,func in formats.items():
-        func(bs)
+#@pytest.mark.parametrize("basis_name", ["6-31G", "6-31GSS", "6-31PPGSS-AGG", "LANL2DZ"])
+#def test_converters(basis_name):
+#
+#    formats = bse.converters.converter_map
+#    bs = bse.get_basis_set(basis_name)
+#
+#    for name,func in formats.items():
+#        func(bs)
