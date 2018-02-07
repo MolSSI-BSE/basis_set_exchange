80f2bf67967be1e55d7eac08e01fa01b1857b429
Benjamin Pritchard <ben@bennyp.org>
2018-02-07 16:20:18 -0500

    Fix LANL2DZ ref and add handling for incollection references

--- bse/refconverters/txt.py
+++ bse/refconverters/txt.py
@@ -6,6 +6,16 @@ def ref_txt(ref):
         s += ', '.join(ref['authors'])
         s += '\n    {}, {}, {} ({})'.format(ref['journal'], ref['volume'], ref['page'], ref['year'])
         s += '\n    ' + ref['doi']
+    elif ref['type'] == 'incollection':
+        s += ', '.join(ref['authors'])
+        s += "\n    {}".format(ref['title'])
+        s += "\n    in \"{}\"".format(ref['booktitle'])
+        if 'editors' in ref:
+            s += '\n    ed. ' + ', '.join(ref['editors'])
+        if 'series' in ref:
+            s += '\n    {}, {}, {} ({})'.format(ref['series'], ref['volume'], ref['page'], ref['year'])
+        if 'doi' in ref:
+            s += '\n    ' + ref['doi']
     else:
         raise RuntimeError("Cannot handle reference type '{}'".format(ref['type']))
     return s
