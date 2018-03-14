bb992e4a4f4a3a02b8d900073f3f94a6381569dc
Benjamin Pritchard <ben@bennyp.org>
2018-03-14 13:32:54 -0400

    Move versions lower in get_metadata

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,27 +6,31 @@ import bse
 import pytest
 
 def test_get_basis_set():
+    # TODO change to nested pytest parametrize
+    # Test combinations of contration options
+    unc_comb = [(False, False, False),
+                (False, False, True),
+                (False, True, False),
+                (False, True, True),
+                (True, False, False),
+                (True, False, True),
+                (True, True, False),
+                (True, True, True)]
+
     bs_metadata = bse.get_metadata()
     formats = bse.get_formats()
 
     for basis_name in bs_metadata.keys():
-        # Test combinations of contration options
-        unc_comb = [(False, False, False),
-                    (False, False, True),
-                    (False, True, False),
-                    (False, True, True),
-                    (True, False, False),
-                    (True, False, True),
-                    (True, True, False),
-                    (True, True, True)]
+        for ver in bs_metadata[basis_name]['versions'].keys():
 
-        # TODO - test getting subsets of elements
-        for f in formats:
-            for unc in unc_comb:
-                bse.get_basis_set(basis_name, elements=None, fmt=f,
-                                  uncontract_general=unc[0],
-                                  uncontract_segmented=unc[1],
-                                  uncontract_spdf=unc[2])
+            # TODO - test getting subsets of elements
+            for f in formats:
+                for unc in unc_comb:
+                    bse.get_basis_set(basis_name, elements=None, fmt=f,
+                                      version=ver,
+                                      uncontract_general=unc[0],
+                                      uncontract_segmented=unc[1],
+                                      uncontract_spdf=unc[2])
 
 
 
