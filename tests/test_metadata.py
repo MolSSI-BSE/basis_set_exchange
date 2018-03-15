a6a646dec44b2eb613cac9c143cf6c7770f738e8
Benjamin Pritchard <ben@bennyp.org>
2018-03-15 16:12:08 -0400

    Fix testing of metadata - hashing is too strict

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,8 +3,8 @@ Tests for BSE metadata
 """
 
 import os
-import hashlib
 import bse
+import json
 from bse import curate
 
 data_dir = bse.default_data_dir
@@ -18,14 +18,12 @@ def test_metadata_uptodate():
     new_metadata = os.path.join(data_dir, 'METADATA.json.new')
     curate.create_metadata_file(new_metadata, data_dir)
 
-    with open(old_metadata, 'rb') as f:
-        old_hash = hashlib.sha1(f.read()).hexdigest()
-    with open(new_metadata, 'rb') as f:
-        new_hash = hashlib.sha1(f.read()).hexdigest()
+    with open(old_metadata, 'r') as f:
+        old_data = json.load(f)
+    with open(new_metadata, 'r') as f:
+        new_data = json.load(f)
 
     os.remove(new_metadata)
 
-    if old_hash != new_hash:
-        print("Old hash: ", old_hash)
-        print("New hash: ", new_hash)
+    if old_data != new_data:
         raise RuntimeError("Metadata does not appear to be up to date")
