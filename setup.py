5089c8a348f44a4741ab94be78f81ce0fbf34c65
Benjamin Pritchard <ben@bennyp.org>
2018-06-20 16:33:14 -0400

    Remove version limit on sphinx

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         ],
         extras_require={
             'docs': [
-                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinx',
                 'sphinxcontrib-napoleon',
                 'sphinx_rtd_theme',
                 'numpydoc',
