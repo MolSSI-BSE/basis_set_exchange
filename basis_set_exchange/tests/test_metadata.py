a55723eba1a33ce8e090d38093d7cf742f8315fd
Benjamin Pritchard <ben@bennyp.org>
2018-09-27 11:07:14 -0400

    Use a true temporary file for test_metadata

--- basis_set_exchange/tests/test_metadata.py
+++ basis_set_exchange/tests/test_metadata.py
@@ -2,6 +2,7 @@
 Tests for BSE metadata
 """
 
+import tempfile
 import json
 import os
 
@@ -9,13 +10,19 @@ from basis_set_exchange import api, curate
 
 _data_dir = api._default_data_dir
 
+
 def test_get_metadata():
     api.get_metadata()
 
 
 def test_metadata_uptodate():
     old_metadata = os.path.join(_data_dir, 'METADATA.json')
-    new_metadata = os.path.join(_data_dir, 'METADATA.json.new')
+
+    # Create a temporary file
+    new_metadata_tmp = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    new_metadata = new_metadata_tmp.name
+    new_metadata_tmp.close()
+
     curate.create_metadata_file(new_metadata, _data_dir)
 
     with open(old_metadata, 'r') as f:
