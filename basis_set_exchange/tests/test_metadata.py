83c963a994336f4ca597adc00b8bd7d0a4fe5e33
Benjamin Pritchard <ben@bennyp.org>
2019-01-27 09:18:07 -0500

    Add missing metadata test

--- basis_set_exchange/tests/test_metadata.py
+++ basis_set_exchange/tests/test_metadata.py
@@ -6,6 +6,7 @@ import tempfile
 import json
 import os
 import pytest
+import glob
 
 from basis_set_exchange import api, curate, fileio
 from .common_testvars import data_dir, all_table_files, all_metadata_files
@@ -40,8 +41,25 @@ def test_metadata_uptodate():
 
 
 @pytest.mark.parametrize('meta_file_path', all_metadata_files)
-def test_basis_metadata(meta_file_path):
+def test_basis_metadata_pair1(meta_file_path):
     '''Test that each metadata file is paired with a table basis
        in the same directory
     '''
-    pass
+
+    bsname = os.path.basename(meta_file_path).split('.')[0]
+    meta_subdir = os.path.dirname(meta_file_path)
+    test_path = os.path.join(data_dir, meta_subdir, bsname)
+    test_files = glob.glob(test_path + '.*.table.json')
+    assert len(test_files) > 0
+
+
+@pytest.mark.parametrize('table_file_path', all_table_files)
+def test_basis_metadata_pair2(table_file_path):
+    '''Test that each table basis is paired with a metadata file
+       in the same directory
+    '''
+
+    bsname = os.path.basename(table_file_path).split('.')[0]
+    table_subdir = os.path.dirname(table_file_path)
+    test_file = os.path.join(data_dir, table_subdir, bsname) + '.metadata.json'
+    assert os.path.isfile(test_file)
