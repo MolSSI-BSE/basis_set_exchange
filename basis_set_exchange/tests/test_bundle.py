aa1e2ab7678bb82846b78cb327089a46da5a2c59
Benjamin Pritchard <ben@bennyp.org>
2019-02-07 21:14:27 -0500

    Even better testing of bundles

--- basis_set_exchange/tests/test_bundle.py
+++ basis_set_exchange/tests/test_bundle.py
@@ -4,14 +4,13 @@ Tests for creating bundles/archives of formatted data
 
 import os
 import tempfile
-import filecmp
 import zipfile
 import tarfile
 import pytest
 import shutil
 import glob
 
-from basis_set_exchange import bundle, converters, refconverters
+from basis_set_exchange import bundle, converters, refconverters, get_basis, get_references
 from .common_testvars import bs_names
 
 
@@ -27,10 +26,11 @@ def _extract_file(filepath, extract_dir):
 
 
 # yapf: disable
+@pytest.mark.parametrize('ext', ['.zip', '.tar.bz2'])
 @pytest.mark.parametrize('fmt, reffmt', [('nwchem', 'bib'),
                                          ('psi4', 'txt')])
 # yapf: enable
-def test_bundles(fmt, reffmt):
+def test_bundles(fmt, reffmt, ext):
     '''Test functionality related to creating archive of basis set'''
 
     exts = ['.zip', '.tar.bz2']
@@ -41,31 +41,33 @@ def test_bundles(fmt, reffmt):
 
     tdir = tempfile.mkdtemp()
 
-    ex_dirs = []
-    for ext in exts:
-        filename = "bundletest_" + fmt + "_" + reffmt + ext
-        filepath = os.path.join(tdir, filename)
+    filename = "bundletest_" + fmt + "_" + reffmt + ext
+    filepath = os.path.join(tdir, filename)
 
-        bundle.create_bundle(filepath, fmt, reffmt)
-        extract_dir = "extract_" + filename
-        extract_path = os.path.join(tdir, extract_dir)
-        _extract_file(filepath, extract_path)
+    bundle.create_bundle(filepath, fmt, reffmt)
+    extract_dir = "extract_" + filename
+    extract_path = os.path.join(tdir, extract_dir)
+    _extract_file(filepath, extract_path)
 
-        ex_dirs.append(extract_path)
+    bs_flist = glob.glob(os.path.join(extract_path, '*' + bs_ext))
+    bs_flist = [x for x in bs_flist if not '.ref.' in x]
+    ref_flist = glob.glob(os.path.join(extract_path, '*.ref' + ref_ext))
 
-    # Check that one file exists per basis
-    for d in ex_dirs:
-        bs_flist = glob.glob(os.path.join(d, '*' + bs_ext))
-        bs_flist = [x for x in bs_flist if not '.ref.' in x]
-        ref_flist = glob.glob(os.path.join(d, '*.ref' + ref_ext))
-        assert len(bs_flist) == nbasis
-        assert len(ref_flist) == nbasis
+    for bs in bs_flist:
+        bs_name = os.path.basename(bs)
+        bs_name = os.path.splitext(bs_name)[0]
+        bs_str = get_basis(bs_name, fmt=fmt)
 
-    # Compare file contents across archive types
-    for d in ex_dirs[1:]:
-        compare = filecmp.dircmp(d, ex_dirs[0])
-        assert len(compare.diff_files) == 0
-        assert len(compare.left_only) == 0
-        assert len(compare.right_only) == 0
+        with open(bs, 'r') as bsf:
+            assert bs_str == bsf.read()
+
+    for ref in ref_flist:
+        bs_name = os.path.basename(ref)
+        bs_name = os.path.splitext(bs_name)[0]
+        bs_name = os.path.splitext(bs_name)[0]  # remove .ref.
+        ref_str = get_references(bs_name, fmt=reffmt)
+
+        with open(ref, 'r') as rf:
+            assert ref_str == rf.read()
 
     shutil.rmtree(tdir)
