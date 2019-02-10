6b6232bc68f14246c235a8eca720f6f3561355ef
Benjamin Pritchard <ben@bennyp.org>
2019-02-10 13:06:22 -0500

    Improve testing of add_basis

--- basis_set_exchange/tests/test_curate2.py
+++ basis_set_exchange/tests/test_curate2.py
@@ -5,7 +5,7 @@ Tests BSE curation functions
 import os
 import tempfile
 
-from basis_set_exchange import curate, api, manip, validator
+from basis_set_exchange import curate, api, validator, misc, fileio, compose, memo
 from .common_testvars import auth_data_dir
 
 # yapf: disable
@@ -14,6 +14,24 @@ _source_data = {'cc-pvdz.1.gbasis.bz2': None,
                 'def2-tzvp.1.tm.bz2': 'myref',
                 'aug-pcj-1.1.gbs.bz2': ['ref1', 'ref2'],
                 'def2-ecp.1.tm.bz2': {'rb': 'ref1', 47: 'ref2', '48': 'ref3', '49-xe': 'ref4'}
+}
+
+_expected_files = {'test_subdir/test_aug-pcj-1_ref1_ref2.1.json': 'H-Ar',
+                   'test_subdir/test_cc-pvdz_noref.1.json':       'H-Ar,Ca-Kr',
+                   'test_subdir/test_def2-ecp_noref.1.json':      'Sr-Pd,Cs-Rn',
+                   'test_subdir/test_def2-ecp_ref1.1.json':       'Rb',
+                   'test_subdir/test_def2-ecp_ref2.1.json':       'Ag',
+                   'test_subdir/test_def2-ecp_ref3.1.json':       'Cd',
+                   'test_subdir/test_def2-ecp_ref4.1.json':       'In-Xe',
+                   'test_subdir/test_def2-tzvp_myref.1.json':     'H-Rn',
+                   'test_subdir/test_def2-tzvp.1.element.json':   'H-Rn',
+                   'test_subdir/test_aug-pcj-1.1.element.json':   'H-Ar',
+                   'test_subdir/test_def2-ecp.1.element.json':    'Rb-Rn',
+                   'test_subdir/test_cc-pvdz.1.element.json':     'H-Ar,Ca-Kr',
+                   'test_aug-pcj-1.1.table.json':                 'H-Ar',
+                   'test_cc-pvdz.1.table.json':                   'H-Ar,Ca-Kr',
+                   'test_def2-ecp.1.table.json':                  'Rb-Rn',
+                   'test_def2-tzvp.1.table.json':                 'H-Rn'
 }
 # yapf: enable
 
@@ -40,10 +58,16 @@ def test_add_basis(tmp_path):
 
         # Compare against the file we created from
         sf_path = os.path.join(auth_data_dir, sf)
-        test_dict = curate.read_formatted_basis(sf_path)
+        assert curate.compare_basis_against_ref(name, sf_path, data_dir=tmp_path) == True
 
-        # Compare, ignoring metadata (not stored in most formats)
-        assert curate.compare_basis(bse_dict, test_dict, rel_tol=0.0)
+        # Check that all the files exist and contain the right elements
+        for fpath, elements in _expected_files.items():
+            fpath = os.path.join(tmp_path, fpath)
+            assert os.path.isfile(fpath)
+
+            fdata = fileio.read_json_basis(fpath)
+            expect_elements = set(misc.expand_elements(elements, True))
+            assert set(fdata['basis_set_elements'].keys()) == expect_elements
 
     # Validate the new data dir
     validator.validate_data_dir(tmp_path)
