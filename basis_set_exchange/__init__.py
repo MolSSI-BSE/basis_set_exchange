88221a3afbf8daa692a344ab7bb7f8d396d547f8
Benjamin Pritchard <ben@bennyp.org>
2019-02-15 11:10:09 -0500

    Make get_archive_types visible to top-level

--- basis_set_exchange/__init__.py
+++ basis_set_exchange/__init__.py
@@ -9,7 +9,7 @@ basis set information
 from .api import (get_basis, lookup_basis_by_role, get_metadata, get_reference_data, get_all_basis_names,
                   get_references, get_basis_family, filter_basis_sets, get_families, get_family_notes, get_basis_notes,
                   get_schema, get_formats, get_reference_formats, get_roles, version)
-from .bundle import create_bundle
+from .bundle import create_bundle, get_archive_types
 
 # Handle versioneer
 from ._version import get_versions
