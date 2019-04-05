3db741f04281e7b26c27733d3e6165052501f658
Benjamin Pritchard <ben@bennyp.org>
2019-04-05 16:35:08 -0400

    Fix missing.py to use aliases

--- basis_set_exchange/tests/sources/missing.py
+++ basis_set_exchange/tests/sources/missing.py
@@ -4,7 +4,7 @@
 # that should be placed in this directory
 
 import os
-from basis_set_exchange import get_metadata
+from basis_set_exchange import get_metadata, misc
 
 my_dir = os.path.dirname(os.path.abspath(__file__))
 auth_sources = [x for x in os.listdir(my_dir) if x.endswith('.bz2')]
@@ -15,22 +15,28 @@ found_ver = {}
 
 # Start with everything in the metadata, removing what we find
 # in this directory
-missing = {k:list(v['versions'].keys()) for k,v in md.items()}
+missing = {k: list(v['versions'].keys()) for k, v in md.items()}
 
 for s in auth_sources:
-    bsname,ver,_,_ = s.split('.')
+    bsname, ver, _, _ = s.split('.')
+    bsname = misc.basis_name_from_filename(bsname)
 
     if not bsname in missing:
         raise RuntimeError("Source {} does not correspond to a basis set in the library".format(bsname))
     if not ver in missing[bsname]:
-        raise RuntimeError("Source {} version {} does not correspond to a basis set version in the library".format(bsname, ver))
+        raise RuntimeError("Source {} version {} does not correspond to a basis set version in the library".format(
+            bsname, ver))
 
-    missing[bsname].remove(ver) 
+    missing[bsname].remove(ver)
 
+    # Also remove aliases
+    for other_name in md[bsname]['other_names']:
+        other_name = misc.transform_basis_name(other_name)
+        missing[other_name].remove(ver)
 
-missing = {k:v for k,v in missing.items() if len(v) > 0}
+missing = {k: v for k, v in missing.items() if len(v) > 0}
 
 maxlen = max([len(k) for k in missing.keys()]) + 5
 
-for k,v in missing.items():
+for k, v in missing.items():
     print('{:{maxlen}} {}'.format(k, ','.join(v), maxlen=maxlen))
