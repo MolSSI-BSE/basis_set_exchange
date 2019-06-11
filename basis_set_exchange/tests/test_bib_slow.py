5aed680672e0df43ca5129d953ed5086717572f6
Benjamin Pritchard <ben@bennyp.org>
2019-06-11 13:43:41 -0400

    Use existing bs_names in test_bibtex

--- basis_set_exchange/tests/test_bib_slow.py
+++ basis_set_exchange/tests/test_bib_slow.py
@@ -6,15 +6,13 @@ import pytest
 from basis_set_exchange import api
 
 from . import check_bibtex
-
-# Load all the metadata once
-_bs_names = api.get_all_basis_names()
+from .common_testvars import bs_names
 
 
 # yapf: disable
 @pytest.mark.slow
 @pytest.mark.skipif(check_bibtex.available is False, reason="Latex/Bibtex commands not found, so I can't verify bib files")
-@pytest.mark.parametrize('basis_name', _bs_names)
+@pytest.mark.parametrize('basis_name', bs_names)
 # yapf: enable
 def test_bibtex(tmp_path, basis_name):
     tmp_path = str(tmp_path)  # Needed for python 3.5
