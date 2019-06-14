8a78dbdd6cd4b1dc8d7ad5aa95ce32acd264294a
Benjamin Pritchard <ben@bennyp.org>
2019-06-14 19:36:54 -0400

    Small improvements to bsedebug output

--- basis_set_exchange/printing.py
+++ basis_set_exchange/printing.py
@@ -81,8 +81,12 @@ def electron_shell_str(shell, shellidx=None):
     coefficients = shell['coefficients']
     ncol = len(coefficients) + 1
 
+    region = shell['region']
+    if region == '':
+        region = '(none)'
+
     point_places = [8 * i + 15 * (i - 1) for i in range(1, ncol + 1)]
-    s = "Shell: {}Region: {}: AM: {}\n".format(shellidx_str, shell['region'], amchar)
+    s = "Shell: {} Region: {} AM: {}\n".format(shellidx_str, region, amchar)
     s += "Function: {}\n".format(shell['function_type'])
     s += write_matrix([exponents, *coefficients], point_places)
     return s
