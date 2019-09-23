3e3d273462ba0491390cb4541f9cfdbacd7ef2de
Benjamin Pritchard <ben@bennyp.org>
2019-09-23 11:06:59 -0400

    Refactor gamess_us format (used for ORCA)

--- basis_set_exchange/converters/gamess_us.py
+++ basis_set_exchange/converters/gamess_us.py
@@ -4,6 +4,71 @@ Conversion of basis sets to GAMESS-US
 
 from .. import lut, manip, sort, printing
 
+def write_gamess_us_electron_basis(basis, electron_elements):
+    # electronic part starts with $DATA
+    s = '$DATA\n'
+
+    for z in electron_elements:
+        data = basis['elements'][z]
+
+        el_name = lut.element_name_from_Z(z).upper()
+        s += '\n' + el_name + "\n"
+
+        for shell in data['electron_shells']:
+            exponents = shell['exponents']
+            coefficients = shell['coefficients']
+            ncol = len(coefficients) + 2  #include index column
+            nprim = len(exponents)
+
+            am = shell['angular_momentum']
+            amchar = lut.amint_to_char(am, hij=True, use_L=True).upper()
+            s += '{}   {}\n'.format(amchar, nprim)
+
+            # 1-based indexing
+            idx_column = list(range(1, nprim + 1))
+            point_places = [0] + [4 + 8 * i + 15 * (i - 1) for i in range(1, ncol)]
+            s += printing.write_matrix([idx_column, exponents, *coefficients], point_places)
+
+    s += "$END"
+    return s
+
+
+def write_gamess_us_ecp_basis(basis, ecp_elements):
+    s = "\n\n$ECP\n"
+
+    for z in ecp_elements:
+        data = basis['elements'][z]
+        sym = lut.element_sym_from_Z(z).upper()
+        max_ecp_am = max([x['angular_momentum'][0] for x in data['ecp_potentials']])
+        max_ecp_amchar = lut.amint_to_char([max_ecp_am], hij=True)
+
+        # Sort lowest->highest, then put the highest at the beginning
+        ecp_list = sorted(data['ecp_potentials'], key=lambda x: x['angular_momentum'])
+        ecp_list.insert(0, ecp_list.pop())
+
+        s += '{}-ECP GEN    {}    {}\n'.format(sym, data['ecp_electrons'], max_ecp_am)
+
+        for pot in ecp_list:
+            rexponents = pot['r_exponents']
+            gexponents = pot['gaussian_exponents']
+            coefficients = pot['coefficients']
+            nprim = len(rexponents)
+
+            am = pot['angular_momentum']
+            amchar = lut.amint_to_char(am, hij=False)
+
+            # Title line
+            if am[0] == max_ecp_am:
+                s += '{:<5} ----- {}-ul potential -----\n'.format(nprim, amchar)
+            else:
+                s += '{:<5} ----- {}-{} potential -----\n'.format(nprim, amchar, max_ecp_amchar)
+
+            point_places = [8, 23, 32]
+            s += printing.write_matrix([*coefficients, rexponents, gexponents], point_places)
+
+    s += "$END\n"
+    return s
+
 
 def write_gamess_us(basis):
     '''Converts a basis set to GAMESS-US
@@ -24,66 +89,10 @@ def write_gamess_us(basis):
 
     # Electron Basis
     if len(electron_elements) > 0:
-        # electronic part starts with $DATA
-        s += '$DATA\n'
-
-        for z in electron_elements:
-            data = basis['elements'][z]
-
-            el_name = lut.element_name_from_Z(z).upper()
-            s += '\n' + el_name + "\n"
-
-            for shell in data['electron_shells']:
-                exponents = shell['exponents']
-                coefficients = shell['coefficients']
-                ncol = len(coefficients) + 2  #include index column
-                nprim = len(exponents)
-
-                am = shell['angular_momentum']
-                amchar = lut.amint_to_char(am, hij=True, use_L=True).upper()
-                s += '{}   {}\n'.format(amchar, nprim)
-
-                # 1-based indexing
-                idx_column = list(range(1, nprim + 1))
-                point_places = [0] + [4 + 8 * i + 15 * (i - 1) for i in range(1, ncol)]
-                s += printing.write_matrix([idx_column, exponents, *coefficients], point_places)
-
-        s += "$END"
+        s += write_gamess_us_electron_basis(basis, electron_elements)
 
     # Write out ECP
     if len(ecp_elements) > 0:
-        s += "\n\n$ECP\n"
-
-        for z in ecp_elements:
-            data = basis['elements'][z]
-            sym = lut.element_sym_from_Z(z).upper()
-            max_ecp_am = max([x['angular_momentum'][0] for x in data['ecp_potentials']])
-            max_ecp_amchar = lut.amint_to_char([max_ecp_am], hij=True)
-
-            # Sort lowest->highest, then put the highest at the beginning
-            ecp_list = sorted(data['ecp_potentials'], key=lambda x: x['angular_momentum'])
-            ecp_list.insert(0, ecp_list.pop())
-
-            s += '{}-ECP GEN    {}    {}\n'.format(sym, data['ecp_electrons'], max_ecp_am)
-
-            for pot in ecp_list:
-                rexponents = pot['r_exponents']
-                gexponents = pot['gaussian_exponents']
-                coefficients = pot['coefficients']
-                nprim = len(rexponents)
-
-                am = pot['angular_momentum']
-                amchar = lut.amint_to_char(am, hij=False)
-
-                # Title line
-                if am[0] == max_ecp_am:
-                    s += '{:<5} ----- {}-ul potential -----\n'.format(nprim, amchar)
-                else:
-                    s += '{:<5} ----- {}-{} potential -----\n'.format(nprim, amchar, max_ecp_amchar)
-
-                point_places = [8, 23, 32]
-                s += printing.write_matrix([*coefficients, rexponents, gexponents], point_places)
-
-        s += "$END\n"
+        s += write_gamess_us_ecp_basis(basis, ecp_elements)
 
     return s
