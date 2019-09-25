4bc0ccef8d5e30054070c729344792948a6326cf
Benjamin Pritchard <ben@bennyp.org>
2019-09-25 16:47:29 -0400

    Add v0 of (aug)-cc-pwCVXZ-RIFIT (X=D,T,Q,5)

--- basis_set_exchange/data/dunning_pp_fit/bse_v0/t.sh
+++
@@ -1,8 +0,0 @@
-bsecurate make-diff -r cc-pV5Z-PP-RIFIT.0.json    -l aug-cc-pV5Z-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pVDZ-PP-RIFIT.0.json    -l aug-cc-pVDZ-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pVQZ-PP-RIFIT.0.json    -l aug-cc-pVQZ-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pVTZ-PP-RIFIT.0.json    -l aug-cc-pVTZ-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pwCV5Z-PP-RIFIT.0.json  -l aug-cc-pwCV5Z-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pwCVDZ-PP-RIFIT.0.json  -l aug-cc-pwCVDZ-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pwCVQZ-PP-RIFIT.0.json  -l aug-cc-pwCVQZ-PP-RIFIT.0.json
-bsecurate make-diff -r cc-pwCVTZ-PP-RIFIT.0.json  -l aug-cc-pwCVTZ-PP-RIFIT.0.json
