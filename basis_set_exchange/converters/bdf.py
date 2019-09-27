ef64d04197533fc066dd609cfbcefcc9ab616b9e
AndyC743 <55843047+AndyC743@users.noreply.github.com>
2019-09-27 19:54:39 +0800

    Create bdf.py
    Conversion of basis sets to BDF format

---
+++ basis_set_exchange/converters/bdf.py
@@ -0,0 +1,82 @@
+'''
+Conversion of basis sets to BDF format
+'''
+
+from .. import lut, manip, printing, misc, sort
+
+
+def write_bdf(basis):
+    '''Converts a basis set to BDF format
+    '''
+
+    basis = manip.make_general(basis, False, True)
+    basis = sort.sort_basis(basis, False)
+
+    s = ''
+
+    # Elements for which we have electron basis
+    electron_elements = [k for k, v in basis['elements'].items() if 'electron_shells' in v]
+
+    # Elements for which we have ECP
+    ecp_elements = [k for k, v in basis['elements'].items() if 'ecp_potentials' in v]
+
+    if len(electron_elements) > 0:
+        # Electron Basis
+        for z in electron_elements:
+            s += '****\n'            
+            data = basis['elements'][z]
+            #Get element symbol
+            s += lut.element_sym_from_Z(z, True)
+            # Since we did make_general, max_am is the number of shells - 1
+            max_am = len(data['electron_shells']) - 1
+            s += '{:>7}   {}\n'.format(z, max_am)
+
+            for shell in data['electron_shells']:
+                exponents = shell['exponents']
+                coefficients = shell['coefficients']
+                nprim = len(exponents)
+                ngen = len(coefficients)
+
+                amchar = lut.amint_to_char(shell['angular_momentum']).upper()
+                s += '{}    '.format(amchar)
+                s += '{:>3}    {}\n'.format(nprim, ngen)
+
+                s += printing.write_matrix([exponents], [17])
+
+                point_places = [8 * i + 15 * (i - 1) for i in range(1, ngen + 1)]
+                s += printing.write_matrix(coefficients, point_places)
+
+    # Write out ECP
+    if len(ecp_elements) > 0:
+        s += '\n\nECP\n'
+
+        for z in ecp_elements:
+            data = basis['elements'][z]
+            sym = lut.element_sym_from_Z(z, True)
+            max_ecp_am = max([x['angular_momentum'][0] for x in data['ecp_potentials']])
+
+            # Sort lowest->highest, then put the highest at the beginning
+            ecp_list = sorted(data['ecp_potentials'], key=lambda x: x['angular_momentum'])
+            ecp_list.insert(0, ecp_list.pop())
+
+            s += '{} nelec {}\n'.format(sym, data['ecp_electrons'])
+
+            for pot in ecp_list:
+                rexponents = pot['r_exponents']
+                gexponents = pot['gaussian_exponents']
+                coefficients = pot['coefficients']
+
+                am = pot['angular_momentum']
+                amchar = lut.amint_to_char(am).upper()
+
+                if am[0] == max_ecp_am:
+                    s += '{} ul\n'.format(sym)
+                else:
+                    s += '{} {}\n'.format(sym, amchar)
+
+                point_places = [0, 10, 33]
+                s += printing.write_matrix([rexponents, gexponents, *coefficients], point_places)
+
+        s += 'END\n'
+
+    return s
