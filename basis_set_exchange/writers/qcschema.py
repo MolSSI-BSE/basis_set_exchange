25d0ef5cf5c2b7976689f682e3a7997c9ed6c0a5
Benjamin Pritchard <ben@bennyp.org>
2020-01-22 15:30:42 -0500

    Handle missing fields in qcschema writer

--- basis_set_exchange/writers/qcschema.py
+++ basis_set_exchange/writers/qcschema.py
@@ -16,12 +16,13 @@ def write_qcschema(basis):
     basis = manip.uncontract_spdf(basis, 1, True)
     basis = sort.sort_basis(basis, True)
 
-    basis_name = basis['name']
+    basis_name = basis.get('name', 'unknown_basis')
+    basis_desc = basis.get('description', '<no description>')
     new_basis = {
         'schema_name': 'qcschema_basis',
         'schema_version': 1,
         'name': basis_name,
-        'description': basis['description']
+        'description': basis_desc
     }
 
     # For the 'center_data' key in the schema
@@ -29,7 +30,8 @@ def write_qcschema(basis):
     for el, eldata in basis['elements'].items():
         entry_name = lut.element_sym_from_Z(el) + '_' + basis_name
 
-        eldata.pop('references')
+        eldata.pop('references', None)
+
         if 'electron_shells' in eldata:
             # Fix up some naming
             for sh in eldata['electron_shells']:
