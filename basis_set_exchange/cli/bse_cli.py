60527b52671c767b4a4791348bee5b5d2c5ab2ed
Susi Lehtola <susi.lehtola@gmail.com>
2021-11-02 09:19:09 -0400

    Add cli wrappers for autoaux and autoabs.

--- basis_set_exchange/cli/bse_cli.py
+++ basis_set_exchange/cli/bse_cli.py
@@ -154,6 +154,29 @@ def run_bse_cli():
         help='Output format (default: autodetected from output filename').completer = cli_write_fmt_completer
 
     #################################
+    # Auxiliary basis sets
+    #################################
+    subp = subparsers.add_parser('autoaux-basis', help='Form AutoAux auxiliary basis')
+    subp.add_argument('input_file', type=str, help='Orbital basis to load')
+    subp.add_argument('output_file', type=str, help='AutoAux basis to write')
+    subp.add_argument(
+        '--in-fmt', type=str, default=None,
+        help='Input format (default: autodetected from input filename').completer = cli_read_fmt_completer
+    subp.add_argument(
+        '--out-fmt', type=str, default=None,
+        help='Output format (default: autodetected from output filename').completer = cli_write_fmt_completer
+
+    subp = subparsers.add_parser('autoabs-basis', help='Form AutoABS auxiliary basis')
+    subp.add_argument('input_file', type=str, help='Orbital basis to load')
+    subp.add_argument('output_file', type=str, help='AutoABS basis to write')
+    subp.add_argument(
+        '--in-fmt', type=str, default=None,
+        help='Input format (default: autodetected from input filename').completer = cli_read_fmt_completer
+    subp.add_argument(
+        '--out-fmt', type=str, default=None,
+        help='Output format (default: autodetected from output filename').completer = cli_write_fmt_completer
+
+    #################################
     # Creating bundles
     #################################
     subp = subparsers.add_parser('create-bundle', help='Create a bundle of basis sets')
