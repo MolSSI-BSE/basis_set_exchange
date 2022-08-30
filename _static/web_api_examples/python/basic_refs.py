fa93ca6ff699d498540d2249437d6ad75d5d515a
bennybp <bennybp@users.noreply.github.com>
2022-08-30 01:26:11 +0000

    Deploying to gh-pages from @ MolSSI-BSE/basis_set_exchange@53eb4ca00b2294bb20b9ec20faf06e680bf110ef 🚀

---
+++ _static/web_api_examples/python/basic_refs.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+
+# This is the main url of the BSE API
+# THIS WILL CHANGE TO HTTPS IN THE FUTURE
+# HTTPS IS RECOMMENDED
+main_bse_url = "http://basissetexchange.org"
+
+# This allows for overriding the URL via an environment variable
+# Feel free to just use the base_url below
+base_url = os.environ.get('BSE_API_URL', main_bse_url)
+
+
+def print_results(r):
+    '''Checks for errors and prints the results of a request'''
+
+    # r.text will contain the formatted output as a string
+    print(r.text)
+    if r.status_code != 200:
+        raise RuntimeError("Could not obtain data from the BSE. Check the error information above")
+
+
+
+############################################
+# Change the user agent and 'from' headers
+############################################
+
+# Change these to something more
+# descriptive if you would like. This lets us know
+# how many different people/groups are using the site
+
+# Valid email is COMPLETELY OPTIONAL. Put whatever
+# you would like in there, or leave it as is. If you
+# do put your email there, we will never give it
+# away or email you, except in case we think errors in
+# your script are causing us problems.
+headers = {
+    'User-Agent': 'BSE Example Python Script',
+    'From': 'bse@molssi.org'
+}
+
+
+# Obtain bibtex references for the same basis set
+r = requests.get(base_url + '/api/references/def2-qzvpd/format/bib',
+                 headers=headers
+                )
+
+print_results(r)
