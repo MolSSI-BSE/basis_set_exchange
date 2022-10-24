d72514a57c8bcbb8ba85a9c0eebd7aefb7743ae2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 07:48:36 +0000

    Adding tarfile member sanitization to extractall()

--- basis_set_exchange/tests/test_bundle.py
+++ basis_set_exchange/tests/test_bundle.py
@@ -50,7 +50,26 @@ def _extract_all(filepath, extract_dir):
             zf.extractall(extract_dir)
     elif filepath.endswith('.tar.bz2'):
         with tarfile.open(filepath, 'r:bz2') as tf:
-            tf.extractall(extract_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, extract_dir)
     else:
         raise RuntimeError("Unexpected file extension")
 
